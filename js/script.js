607c6ccf4b56e6edd98c10384a73f337d035487d
shalizsharafi <shalizoputh@gmail.com>
2026-08-02 22:54:11 -0700

    the bottom of the player verses the velocity is less than bottom of our canvas right here then we want to add gravity on to it. but the second the playuer reached and passed the bottom of the screen we want to set the velocity to 0

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const canvas = document.querySelector('canvas')
 
 //game setup ++++++++++++_______((())()()(_(_(__(__(_()(_)))))))?????????////////////////////////////////////////////////////
 const context = canvas.getContext('2d')
-
+const gravity = 0.5
 
 //make the canvas squared
 canvas.width = innerWidth
@@ -18,9 +18,10 @@ class Player{
                      x:100,
                      y:100
               }
+              //per px the player moves
               this.velocity={
                      x:0,
-                     y:1
+                     y:0
               }
               this.width=30
               this.height=30
@@ -36,7 +37,12 @@ class Player{
        //changes our player's properties over time which separates what the player actually looks like with what we're actually updating over time
        update(){
               this.draw()
+
               this.position.y += this.velocity.y
+              //the bottom of the player verses the velocity is less than bottom of our canvas right here then we want to add gravity on to it. but the second the playuer reached and passed the bottom of the screen we want to set the velocity to 0
+              if(this.position.y + this.height + this.velocity.y <= canvas.height) this.velocity.y += gravity
+               //excellerating over time, makes the speed higher, excellartaion essociated with the velocity
+               else this.velocity.y = 0
        }
 }
 //creating the player inside the canvas
@@ -55,3 +61,15 @@ function animate(){
 }
 
 animate()
+
+
+
+addEventListener('keydown',({keyCode})=>{
+       console.log(keyCode)
+       switch(keyCode){
+              case 37 : console.log('left') ; break;
+              case 38 : console.log('up') ; break;
+              case 39 : console.log('right');break;
+              case 40 : console.log('down') 
+       }
+})
